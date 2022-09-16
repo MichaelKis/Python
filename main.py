9102b27502064813d7ae74d2ef5d1a89b3d5fad6
Миша <mitchell1@mail.ru>
2022-09-16 18:01:29 +0300

    Добавлена очистка файла csv перед сохранением Добавлен заголовок

--- main.py
+++ main.py
@@ -3,15 +3,20 @@ from bs4 import BeautifulSoup
 import pandas
 import csv
 
-# f = open('stoloto.csv','w')
-# f.close()
+file = open("stoloto.csv", "w")
+file.truncate()
+file.close()
 
+count = 0  # "счетчик для заголовка"
 
-for num in range(1454,1458):
+for num in range(1456, 1458):
 
     url = f'https://www.stoloto.ru/ruslotto/archive/{num}'
     r = requests.get(url)
-    print(r.status_code)
+    status=r.status_code
+
+    # if status != 200:
+    #     raise ValueError ("Oops!  Server don`t ask.  Try again...")
 
     soup = BeautifulSoup(r.text, "html.parser")
 
@@ -21,11 +26,18 @@ for num in range(1454,1458):
 
     for number in res.find_all('span'):
         new_str.append(int(number.text))
-
-    with open('stoloto.csv', 'a', newline='') as f:
-        writer = csv.writer(f, delimiter=',', quoting=csv.QUOTE_ALL)
-        #writer = csv.writer(f)
+    count +=1
+    with open('stoloto.csv', 'a', newline='') as file:
+        fieldnames = ["Number", '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16',
+                      '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32',
+                      '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48',
+                      '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64',
+                      '65', '66', '67', '68', '69', '70', '71', '72', '73', '74', '75', '76', '77', '78', '79', '80',
+                      '81', '82', '83', '84', '85', '86', '87', '88', '89', '90']
+        writer = csv.DictWriter(file, fieldnames=fieldnames, delimiter=',', quoting=csv.QUOTE_ALL)
+        if count < 2:
+            writer.writeheader()
+        writer = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
         writer.writerow(new_str)
 
     print(new_str)
-
