96f178cc5e26e5e7af38d4a06b35f814ce45cf6f
Миша <mitchell1@mail.ru>
2022-11-05 16:22:54 +0300

    Новые тиражи

--- statist.py
+++ statist.py
@@ -1,19 +1,31 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+number_list=[]
+
 data = pd.read_csv('stoloto.csv')
 
 df = pd.DataFrame(data)
 
 del df['Number']
 
-a = df[['1']].value_counts()[:10]
+a1 = df[['1']].value_counts()[:5]
 
+a2 = df[['2']].value_counts()[:5]
 
+a3 = df[['3']].value_counts()[:5]
 
-print(a)
+a4 = df[['4']].value_counts()[:5]
 
-a.plot()
+a5 = df[['5']].value_counts()[:5]
+
+a6 = df[['6']].value_counts()[:5]
+
+a7 = df[['7']].value_counts()[:5]
+
+print(a1,a2,a3,a4,a5,a6,a7)
+
+a1.plot()
 plt.show()
 
 #1=87
