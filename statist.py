f1adf917e8559795331045b48dbe81e0797d4478
Миша <mitchell1@mail.ru>
2022-11-05 15:43:53 +0300

    Новые тиражи

--- statist.py
+++ statist.py
@@ -7,18 +7,19 @@ df = pd.DataFrame(data)
 
 del df['Number']
 
-a = df[['1','2']].value_counts()
+a = df[['1']].value_counts()[:10]
 
 
 
 print(a)
 
-# a.plot()
-# plt.show()
+a.plot()
+plt.show()
 
 #1=87
 #2=67,88,80,12
 #3=23,13,3,8
-#4=44,1,62,89
+#4=44
 #5=59
-#6=40
+#6=40,29,33,34,39
+#7=41
